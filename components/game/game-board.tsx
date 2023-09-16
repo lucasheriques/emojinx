c36bf4b0b97bc54bd98f34c8026ba3f8df957655
Lucas Henrique <lucasheriques@gmail.com>
2023-09-16 20:12:11 -0300

    some additional refactoring

--- components/game/game-board.tsx
+++ components/game/game-board.tsx
@@ -5,25 +5,11 @@ import useGame from "@/components/game/hooks/use-game";
 import useMakeMove, { MoveArgs } from "@/components/game/hooks/use-make-move";
 import usePlayerId from "@/components/game/hooks/use-player-id";
 import NoInternetBanner from "@/components/game/no-internet-banner";
-import { Button } from "@/components/ui/button";
 import { GameStatus } from "@/convex/types";
 import { useConvex } from "convex/react";
-import { useTheme } from "next-themes";
 import useCountdownEffect from "./hooks/use-countdown-effect";
 import useGameCheckEffect from "./hooks/use-game-check-effect";
-import { useState } from "react";
-
-const gridSizes: {
-  [key: number]: string;
-} = {
-  8: "grid-cols-4",
-  16: "grid-cols-4",
-  36: "grid-cols-4 sm:grid-cols-6",
-  64: "grid-cols-4 sm:grid-cols-8",
-  100: "grid-cols-5 sm:grid-cols-10",
-};
-
-const flippedStatus = ["revealed", "matched"];
+import Grid from "./grid";
 
 export default function GameBoard() {
   const game = useGame();
@@ -70,91 +56,5 @@ export default function GameBoard() {
       <Grid emojiList={emojiListWithDisabledStatus} />
       <DeleteGameButton />
     </div>
-  );
-}
-
-function Grid({
-  emojiList,
-}: {
-  emojiList: {
-    status: string;
-    value: string;
-    disabled: boolean;
-    handleMove: (args: MoveArgs) => Promise<
-      | {
-          move: "first";
-        }
-      | {
-          move: "second";
-          isGameFinished: boolean;
-          winnerIds: string[];
-          matched: boolean;
-          firstEmojiIndex: number;
-          secondEmojiIndex: number;
-        }
-    >;
-  }[];
-}) {
-  const { resolvedTheme } = useTheme();
-  const [lastPairSelected, setLastPairSelected] = useState<{
-    firstEmojiIndex: number;
-    secondEmojiIndex: number;
-    matched: boolean;
-  } | null>(null);
-
-  return (
-    <ul
-      className={`grid ${
-        gridSizes[emojiList.length]
-      } gap-x-2 sm:gap-x-4 gap-y-4`}
-    >
-      {emojiList.map(({ status, disabled, handleMove, value }, index) => {
-        const wasSelected =
-          lastPairSelected?.firstEmojiIndex === index ||
-          lastPairSelected?.secondEmojiIndex === index;
-
-        const wasMatched = wasSelected && lastPairSelected?.matched;
-        return (
-          <li
-            key={index}
-            className={`flex justify-center card rounded ${
-              flippedStatus.includes(status) ? "is-flipped" : "is-down"
-            }
-            ${
-              wasSelected
-                ? wasMatched
-                  ? "animate-matched-outline"
-                  : "animate-failed-outline"
-                : ""
-            }
-            `}
-          >
-            <Button
-              variant="outline"
-              className={`md:text-4xl text-red text-3xl py-6 px-3 md:px-4 disabled:opacity-100 
-              disabled:cursor-not-allowed disabled:pointer-events-auto`}
-              disabled={disabled}
-              onClick={async () => {
-                const newStatus = await handleMove({ index });
-
-                if (newStatus.move === "first") {
-                  setLastPairSelected(null);
-                  return;
-                }
-
-                setLastPairSelected({
-                  firstEmojiIndex: newStatus.firstEmojiIndex,
-                  secondEmojiIndex: newStatus.secondEmojiIndex,
-                  matched: newStatus.matched,
-                });
-              }}
-            >
-              {status === "hidden" && (resolvedTheme === "dark" ? "❔" : "❓")}
-              {status !== "hidden" && value}
-            </Button>
-          </li>
-        );
-      })}
-    </ul>
   );
 }
---
+++ components/game/grid.tsx
@@ -0,0 +1,112 @@
+import { useTheme } from "next-themes";
+import { MoveArgs } from "./hooks/use-make-move";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+const gridSizes: {
+  [key: number]: string;
+} = {
+  8: "grid-cols-4",
+  16: "grid-cols-4",
+  36: "grid-cols-4 sm:grid-cols-6",
+  64: "grid-cols-4 sm:grid-cols-8",
+  100: "grid-cols-5 sm:grid-cols-10",
+};
+
+const flippedStatus = ["revealed", "matched"];
+
+type currentStatus =
+  | {
+      move: "first";
+    }
+  | {
+      move: "second";
+      isGameFinished: boolean;
+      winnerIds: string[];
+      matched: boolean;
+      firstEmojiIndex: number;
+      secondEmojiIndex: number;
+    };
+
+type GridProps = {
+  emojiList: {
+    status: string;
+    value: string;
+    disabled: boolean;
+    handleMove: (args: MoveArgs) => Promise<currentStatus>;
+  }[];
+};
+
+export default function Grid({ emojiList }: GridProps) {
+  const { resolvedTheme } = useTheme();
+  const [lastPairSelected, setLastPairSelected] = useState<{
+    firstEmojiIndex: number;
+    secondEmojiIndex: number;
+    matched: boolean;
+  } | null>(null);
+  const [currentStatus, setCurrentStatus] = useState<currentStatus | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (!currentStatus || currentStatus.move === "first") {
+      setLastPairSelected(null);
+      return;
+    }
+
+    setLastPairSelected({
+      firstEmojiIndex: currentStatus.firstEmojiIndex,
+      secondEmojiIndex: currentStatus.secondEmojiIndex,
+      matched: currentStatus.matched,
+    });
+  }, [currentStatus]);
+
+  console.log({ lastPairSelected });
+
+  return (
+    <ul
+      className={`grid ${
+        gridSizes[emojiList.length]
+      } gap-x-2 sm:gap-x-4 gap-y-4`}
+    >
+      {emojiList.map(({ status, disabled, handleMove, value }, index) => {
+        const wasJustSelected =
+          lastPairSelected?.firstEmojiIndex === index ||
+          lastPairSelected?.secondEmojiIndex === index;
+
+        let className = "";
+
+        if (flippedStatus.includes(status)) {
+          className += "is-flipped ";
+        }
+
+        if (wasJustSelected) {
+          className += lastPairSelected.matched
+            ? "animate-matched-outline"
+            : "animate-failed-outline";
+        }
+        return (
+          <li
+            key={index}
+            className={`flex justify-center card rounded ${className}`}
+          >
+            <Button
+              variant="outline"
+              className={`md:text-4xl text-red text-3xl py-6 px-3 md:px-4 disabled:opacity-100 
+              disabled:cursor-not-allowed disabled:pointer-events-auto`}
+              disabled={disabled}
+              onClick={async () => {
+                const newStatus = await handleMove({ index });
+
+                setCurrentStatus(newStatus);
+              }}
+            >
+              {status === "hidden" && (resolvedTheme === "dark" ? "❔" : "❓")}
+              {status !== "hidden" && value}
+            </Button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
